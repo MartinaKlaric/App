be8baaf224f0e0313002a8e94006d85345e24bdb
Martina Klarić <martinaklaric1103gmail.com>
2023-03-22 16:35:43 +0000

    vjezbe15.3-json

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
 
-echo phpinfo();
+$polaznici=file_get_contents(__DIR__.'/polaznici.json'); //čitanje sadržaja datoteke
+$polazniciNiz=json_decode($polaznici, true); //prebacivanje u niz
+?>
+
+<!--Ispis podataka u tablicu -->
+<table border="1" cellpadding="10">
+        <tr>
+            <th>Ime</th>
+            <th>Prezime</th>
+            <th>Godine</th>
+            <th>Email</th>
+            <th>Telefon</th>
+        </tr>
+        <?php
+           foreach($polazniciNiz as $polaznik){
+            echo '<tr>';
+            echo '<td>'. $polaznik['name']. '</td>';
+            echo '<td>'. $polaznik['surname']. '</td>';
+            echo '<td>'. $polaznik['age']. '</td>';
+            echo '<td>'. $polaznik['email']. '</td>';
+            echo '<td>'. $polaznik['phone']. '</td>';
+            echo '</tr>';
+           }
+        ?>
+
+    </table> <!--Gotovo prebacivanje podataka u tablicu -->
+
+<?php
+    //Dodavanje novog polaznika
+    $polazniciNiz[]=[
+        "name"=>"Ivan",
+        "surname"=>"Mandić",
+        "age"=> 30,
+        "email"=>"ivan.mandic@gmail.com",
+        "phone"=> 38599888999,
+
+    ];
+
+//Transformiranje u JSON
+$polazniciJson = json_encode($polazniciNiz);
+//Zapisivanje novih podataka u datoteku
+file_put_contents(__DIR__.'/polaznici.json', $polazniciJson);
 
---
+++ obrazac.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        <table border="1">
+        <tr>
+            <th>Ime:</th>
+            <td><input type="text" name="firstname"></td>
+        </tr>
+
+        <tr>
+            <th>Prezime:</th>
+            <td><input type="text" name="lastname"></td>
+        </tr>
+
+        <tr>
+            <th>Email:</th>
+            <td><input type="text" name="email"></td>
+        </tr>
+
+        <tr>
+            <th>Lozinka:</th>
+            <td><input type="password" name="password"></td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <button type="submit">Pošalji!</button>
+            
+          </td>
+        </tr>
+        </table>
+    </form>
+</body>
+</html>
