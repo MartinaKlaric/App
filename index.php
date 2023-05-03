deea8337366b5e35949e67c8561ac92bb2f74d24
Martina Klarić <martinaklaric1103gmail.com>
2023-05-03 16:14:51 +0000

    Vježba -> Apstrakcija

--- index.php
+++ index.php
@@ -1,62 +1,62 @@
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parcijalni ispit - osnove PHP-a</title>
-</head>
-<body>
-    <div class="center">
-        <?php
-        if($_SESSION['error message']==NULL){
-            echo '<div><h1>Upišite riječ: </h1></div>';
-        } elseif($_SESSION['error message']=='Empty'){
-            echo '<div><h1>Polje ne smije biti prazno</h1></div>';
-        } elseif($_SESSION['error message']=='NonLetterUsed'){
-            echo '<div><h1>Treba koristiti samo slova.</h1></div>';
-        }
+interface Vehicle
+{
+   public function start():void;
+   public function stop():void;
+}
 
-        ?>
-    </div>
+abstract class Car
+{
+   public function __construct(protected int $tankCapacity){}
+   abstract function getKmOnFullTank():int;
+}
 
-    <div class="flex-container">
-        <div id="form">
-            <form action="provjera.php" method="post">
-                <label>Upišite riječ:</label>
-                <br>
-                <input type="text" name="word">
-                <br>
-                <input type="submit" value="pošalji">
-    </form>
-    </div>
-    <div>
-        <?php
-        $wordsJson=file_get_contents('words.json');
-        $words=json_decode($wordsJson, true);
-        ?>
 
-        <table border="1", cellpadding="5">
-            <tr>
-                <th>Riječ</th>
-                <th>Broj slova</th>
-                <th>Broj suglasnika</th>
-                <th>Broj samoglasnika</th>
-            </tr>    
-        <?php
-        foreach($words as $word){
-            echo "<tr>";
-            echo    "<td>{$word['word']}</td>";
-            echo    "<td>{$word['noChar']}</td>";
-            echo    "<td>{$word['noCon']}</td>";
-            echo    "<td>{$word['noVow']}</td>";
-            echo "</tr>";
-        }
-        ?>
-    </table>
-    </div>
-    </div>           
+class Kia extends Car implements Vehicle
+{
+   public function getKmOnFullTank():int
+   {
+      return $this->tankCapacity*18;
+   }
+   public function start(): void
+   {
+      echo "Kia has started.\n";
+   }
+   public function stop(): void
+   {
+      echo "Kia has stopped.\n";
+   }
+}
 
-</body>
-</html>
+class Audi extends Car implements Vehicle
+{
+   public function getKmOnFullTank():int
+   {
+      return $this->tankCapacity*20;
+   }
+   public function start(): void
+   {
+      echo "Audi has started.\n";
+   }
+   public function stop(): void
+   {
+      echo "Audi has stopped.\n";
+   }
+}
+
+class Avion implements Vehicle
+{
+   public function start(): void
+   {
+      echo "Avion has started.\n";
+   }
+   public function stop(): void
+   {
+      echo "Avion has stopped.\n";
+   }
+}
+
+
+
+   
